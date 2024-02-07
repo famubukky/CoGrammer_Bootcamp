d98fffc001cd81f571622e49daf6e0d9f7356494
Alaba Ogungbite <famubukky@famubukky@gmail.com>
2024-02-07 23:33:13 +0000

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,35 @@
+#The line of code that shows that user should enter the first integer 
+user_1 = int(input("Enter the first interger:  "))
+
+#The line of code that shows that user should enter the second integer 
+user_2 = int(input("Enter the second integer:  "))
+
+#The line of code that shows that user should enter the third integer 
+user_3 = int(input("Enter the third interger:  "))
+
+#calculate the sum of all intergers entered by the users
+Total_sum = user_1 + user_2 + user_3
+
+#print the sum of all intergers entered by the users
+print("The total sum of all users: ", Total_sum)
+
+
+#calculate the difference of values enter by user_1 and user_2 
+diff_user_1_user_2 = user_1 - user_2
+
+#print the difference of values enter by user_1 and user_2 
+print("The different in user_1 and user_2: ",  diff_user_1_user_2)
+
+#calculate the division values of all users by user_3 
+division_all_users = Total_sum / user_3
+
+
+#print the division values of all users by user_3 
+print("The division numbers of all users: ", division_all_users)
+
+
+
+
+
+
+
