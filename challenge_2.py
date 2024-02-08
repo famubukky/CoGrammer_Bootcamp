298b439591c3aed07352453b3f560e76d3674459
Alaba Ogungbite <famubukky@famubukky@gmail.com>
2024-02-08 00:26:32 +0000

    challenge

---
+++ challenge_2.py
@@ -0,0 +1,24 @@
+#The line of code thats show how will take the name of the user and store in variable called string_fav
+string_fav = input("Enter your favourite restaurant:  ")
+
+#ask the user his favourite number and cast it into integer
+int_fav = input("Enter your favourite number: ")
+
+# Take the name of the user's favourite restaurant
+string_fav = input("Enter your favourite restaurant: ")
+
+# Take the user's favourite number and cast it to an integer
+int_fav = int(input("Enter your favourite number: "))
+
+# Print out the favourite restaurant and favourite number
+print("Your favourite restaurant:", string_fav)
+print("Your favourite number:", int_fav)
+
+#line of code that shows how we are trying to cast string_fav into integer
+string_fav = int(input("Enter your favourite resturant: "))
+
+#The code is given error, because it is not possible to cast string to Integer due to non-numeric characters in string
+
+
+
+
