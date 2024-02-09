8f0c9950fcadcebce402bd0a575d32382b24cefc
Alaba Ogungbite <famubukky@famubukky@gmail.com>
2024-02-09 22:55:11 +0000

    ifExample

--- ifExample.bat
+++ ifExample.bat
@@ -1,9 +1,12 @@
-:: line of code that shows if folder named new_folder exist
+:: Check if folder named new_folder exist
+:: if new_folder exist, create a folder called if_folder
+:: if if_folder exist, create a folder called HyperionDev 
+:: if if_folder doesnt exist, create a folder called new-projects
 if  exist "new_folder" (
     mkdir if_folder
 )
 if exist "if_folder" (
-    mkdir hyperionDev
+    mkdir HyperionDev
 )else (
     mkdir new-projects
 )
